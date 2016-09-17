83f9046ca4bf80daebb40541cdb432b5c0cd8935
Chris <csherling@stuy.edu>
2016-09-17 17:29:14 -0400

    did 004

---
+++ 004/4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+  int i;
+  int j;
+  int curnum = 0;
+  for(i = 999; i >= 100; i--){
+    for(j = 999; j >= 100; j--){
+      curnum = i*j;
+      if(curnum > 100000){
+
+	if(curnum / 100000 == curnum % 10){
+
+	  if(curnum / 10000 % 10 == curnum % 100 / 10){
+
+	    if(curnum / 1000 % 10 == curnum % 1000 / 100){
+	      printf("result: %d \n", curnum);
+	      return;
+	    }
+	  }
+	}
+      }
+    } 
+  }
+
+}
